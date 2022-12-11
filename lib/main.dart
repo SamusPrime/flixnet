43813f9cc6bc2725c54b49f5d7f935d3497c3ba7
Sebastian Augusto Montero <cheva_mont@live.com.ar>
2022-12-11 15:39:06 -0300

    created homescreen, added lints

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'presentation/view/home_screen.dart';
+
+import 'core/util/string_constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: StringConstants.mainTitle,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/presentation/widgets/custom_app_bar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../../core/util/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  final double scrollOfSet;
+
+  const CustomAppBar({Key? key, this.scrollOfSet = 0.0}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 20.0,
+      ),
+      color: Colors.red.withOpacity(
+        (scrollOfSet / 350).clamp(0, 1).toDouble(),
+      ),
+      child: SafeArea(
+        child: Row(
+          children: [
+            Image.asset(Assets.netflixLogo0),
+            const SizedBox(
+              width: 12.0,
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _AppBarButton(
+                    title: 'Tv Shows',
+                    onTap: () => print('tv shows'),
+                  ),
+                  _AppBarButton(
+                    title: 'Movies',
+                    onTap: () => print('Movies'),
+                  ),
+                  _AppBarButton(
+                    title: 'My list',
+                    onTap: () => print('My list'),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AppBarButton extends StatelessWidget {
+  final String title;
+  final Function onTap;
+
+  const _AppBarButton({
+    Key? key,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap(),
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
